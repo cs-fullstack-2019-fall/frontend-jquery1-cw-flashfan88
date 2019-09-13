75cbc2b406f445ad6d72b1b18c7728328d967335
autumn-ragland <raglanda998@gmail.com>
2019-09-12 20:25:47 -0500

    Graded 09-12-2019 20:25:14

--- grade.js
+++ grade.js
@@ -1,3 +1,6 @@
+// !! : you have to link jQuery in your HTML
+
+// !! : assign color attriubutes using jquery 
 $(document).ready(function(){
     $("#myElement").click(function(){
         alert("myElement was clicked.");
